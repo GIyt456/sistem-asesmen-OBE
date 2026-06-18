ed1900df5d1085b85b2af84de857eb82ef10932b
unknown <georgiuskrisna004@gmail.com>
2026-06-18 14:22:36 +0700

    Pembaruan semuanya

--- web_frontend_akademik/js/api_config.js
+++ web_frontend_akademik/js/api_config.js
@@ -1,3 +1,3 @@
 // MASTER LINK CLOUDFLARE TUNNEL N8N
-const API_BASE = "https://contained-reply-followed-integrate.trycloudflare.com/webhook";
+const API_BASE = "https://highway-hill-hundreds-andale.trycloudflare.com/webhook";
 
