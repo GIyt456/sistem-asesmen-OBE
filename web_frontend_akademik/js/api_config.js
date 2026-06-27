4a84fc04d0e044ec942823e473e90ec72503c6a7
Christian <christian.abelwijaya@gmail.com>
2026-06-28 01:23:53 +0700

    kaprodi mobile

--- web_frontend_akademik/js/api_config.js
+++ web_frontend_akademik/js/api_config.js
@@ -1,6 +1,2 @@
 // MASTER LINK CLOUDFLARE TUNNEL N8N
-<<<<<<< HEAD
 const API_BASE = "https://n8n.obeyogabeta.net/webhook";
-=======
-const API_BASE = "https://n8n.obeyogabeta.net/webhook";
->>>>>>> 96d8925b6ca6db306a7fd4f0759c1dbbb40e87e8
